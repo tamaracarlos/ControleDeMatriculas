fe0f07662f0d237d9b6bb8ee5d6da17f2261d8cc
tamaracarlos <tamaraselau@hotmail.com>
2016-09-29 18:20:36 -0300

    Criado select de Alunos e Cursos em Matrículas;

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem "therubyracer"
 gem "less-rails" 
 gem "twitter-bootstrap-rails"
 gem 'cpf_validator', '~> 0.2.1'
+gem 'rails-i18n', '~> 4.0.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,6 +1,6 @@
 class Aluno < ActiveRecord::Base
-	validates_presence_of :cpf, :rg, :data_nascimento, :nome, :telefone, message: 'deve ser preenchido'
-	
+	validates_presence_of :cpf, :rg, :nome, :telefone, message: 'deve ser preenchido'
+
 	validates :cpf, :cpf => true
 	validates :cpf, 
 					length: {is: 11, message: 'deve conter 11 números'},
@@ -11,5 +11,14 @@ class Aluno < ActiveRecord::Base
 					
 	validates :rg, 
 					length: {maximum: 10, message: 'deve conter no máximo 10 números'}
-					
+
+	# validates_presence_of :data_nascimento, :if => :ano_bissexto?
+			
+	 	
+	# def ano_bissexto? 
+	# 	if data_nascimento == "2008-02-02"
+	# 	  puts "Ano Bissexto"		   
+	# 	end
+	# end
+
 end
